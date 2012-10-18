408063e7457f9e0dd3bd3b93ed356e7217822bdb
benas <md.benhassine@gmail.com>
2012-10-19 00:52:22 +0200

    add CB4J JMX MBean

---
+++ cb4j-core/src/main/java/net/benas/cb4j/core/jmx/BatchMonitor.java
@@ -0,0 +1,106 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2012, benas (md.benhassine@gmail.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.benas.cb4j.core.jmx;
+
+import net.benas.cb4j.core.api.BatchReporter;
+
+import java.util.Date;
+
+/**
+ *  CB4J JMX MBean implementation of {@link BatchMonitorMBean}
+ *  @author benas (md.benhassine@gmail.com)
+ */
+public class BatchMonitor implements BatchMonitorMBean {
+
+    /**
+     * The batch reporter holding data exposed as JMX attributes
+     */
+    private BatchReporter batchReporter;
+
+    public BatchMonitor(BatchReporter batchReporter) {
+        this.batchReporter = batchReporter;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getInputRecordsNumber() {
+        return batchReporter.getInputRecordsNumber();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getTotalInputRecordsNumber() {
+        return batchReporter.getTotalInputRecordsNumber();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getIgnoredRecordsNumber() {
+        return batchReporter.getIgnoredRecordsNumber();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long getRejectedRecordsNumber() {
+        return batchReporter.getRejectedRecordsNumber();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getStartTime() {
+        return new Date(batchReporter.getStartTime()).toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getEndTime() {
+        long endTime = batchReporter.getEndTime();
+        return (endTime == 0) ? "" : new Date(endTime).toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getBatchStatus() {
+        return batchReporter.getBatchStatus().toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String getProgress() {
+        long inputRecordsNumber = getInputRecordsNumber();
+        long totalInputRecordsNumber = getTotalInputRecordsNumber();
+        String ratio = inputRecordsNumber + "/" + totalInputRecordsNumber;
+        String percent = " (" + ( (int) (( (float) inputRecordsNumber / (float) totalInputRecordsNumber ) * 100) ) + "%)";
+        return ratio + percent;
+    }
+}
---
+++ cb4j-core/src/main/java/net/benas/cb4j/core/jmx/BatchMonitorMBean.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ *  Copyright (c) 2012, benas (md.benhassine@gmail.com)
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+package net.benas.cb4j.core.jmx;
+
+/**
+ *  CB4J JMX MBean interface
+ *  @author benas (md.benhassine@gmail.com)
+ */
+public interface BatchMonitorMBean {
+
+    /**
+     * Get input records number.
+     * @return input records number
+     */
+    long getInputRecordsNumber();
+
+    /**
+     * Get total input records number.
+     * @return total input records number
+     */
+    long getTotalInputRecordsNumber();
+
+    /**
+     * Get ignored records number.
+     * @return ignored records number
+     */
+    long getIgnoredRecordsNumber();
+
+    /**
+     * Get rejected records number.
+     * @return rejected records number
+     */
+    long getRejectedRecordsNumber();
+
+    /**
+     * Get batch execution start time.
+     * @return batch execution start time
+     */
+    String getStartTime();
+
+    /**
+     * Get batch execution end time.
+     * @return batch execution end time
+     */
+    String getEndTime();
+
+    /**
+     * Get batch execution status.
+     * @return batch execution status
+     */
+    String getBatchStatus();
+
+    /**
+     * Get a human readable batch execution progress.
+     * @return a human readable batch execution progress
+     */
+    String getProgress();
+
+}
