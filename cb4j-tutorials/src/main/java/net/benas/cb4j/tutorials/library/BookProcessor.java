6993dee0966f49f851d52fb7bfddb035d11dc417
benas <md.benhassine@gmail.com>
2013-08-31 13:05:06 +0200

    update library tutorial : commit transaction in processRecord method

--- cb4j-tutorials/src/main/java/net/benas/cb4j/tutorials/library/BookProcessor.java
+++ cb4j-tutorials/src/main/java/net/benas/cb4j/tutorials/library/BookProcessor.java
@@ -1,9 +1,8 @@
 package net.benas.cb4j.tutorials.library;
 
 import net.benas.cb4j.core.api.RecordProcessingException;
-import net.benas.cb4j.core.api.RecordProcessor;
+import net.benas.cb4j.core.impl.DefaultRecordProcessorImpl;
 import org.hibernate.HibernateException;
-import org.hibernate.Transaction;
 
 /**
  * A book processor that inserts Book objects in database.<br/>
@@ -13,7 +12,7 @@ import org.hibernate.Transaction;
  *
  * @author benas (md.benhassine@gmail.com)
  */
-public class BookProcessor implements RecordProcessor<Book> {
+public class BookProcessor extends DefaultRecordProcessorImpl<Book> {
 
     public void preProcessRecord(Book book) throws RecordProcessingException {
         DatabaseUtil.getCurrentSession().beginTransaction();
@@ -22,16 +21,10 @@ public class BookProcessor implements RecordProcessor<Book> {
     public void processRecord(Book book) throws RecordProcessingException {
         try {
             DatabaseUtil.getCurrentSession().saveOrUpdate(book);
+            DatabaseUtil.getCurrentSession().getTransaction().commit();
         } catch (HibernateException e) {
             DatabaseUtil.getCurrentSession().getTransaction().rollback();
             throw new RecordProcessingException(e.getMessage());
-        }
-    }
-
-    public void postProcessRecord(Book book) throws RecordProcessingException {
-        Transaction transaction = DatabaseUtil.getCurrentSession().getTransaction();
-        if (transaction.isActive() && !transaction.wasRolledBack()) {
-            transaction.commit();
         }
     }
 
