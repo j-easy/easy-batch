9bd434a7399151aeb38439f72c4a023fbcbc8737
benas <md.benhassine@gmail.com>
2013-08-31 12:59:44 +0200

    Add BatchResultHolder class to hold batch execution result. This is useful when you need to return some computation results at the end of batch execution.

---
+++ cb4j-core/src/main/java/net/benas/cb4j/core/api/BatchResultHolder.java
@@ -0,0 +1,15 @@
+package net.benas.cb4j.core.api;
+
+/**
+ * Batch execution result holder.
+ *
+ * @author benas (md.benhassine@gmail.com)
+ */
+public interface BatchResultHolder<R> {
+
+    /**
+     * Get batch execution result.
+     */
+    public R getResult();
+
+}
