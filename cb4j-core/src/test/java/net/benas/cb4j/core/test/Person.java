3072d38943fe884fba6ec4f406719090f9bb7b02
benas <md.benhassine@gmail.com>
2013-09-08 21:37:31 +0200

    make Person class public

---
+++ cb4j-core/src/test/java/net/benas/cb4j/core/test/Person.java
@@ -0,0 +1,35 @@
+package net.benas.cb4j.core.test;
+
+public class Person {
+
+    private String firstName;
+
+    private String lastName;
+
+    void Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
--- cb4j-core/src/test/java/net/benas/cb4j/core/test/ReflectionPerformanceShowcase.java
+++ cb4j-core/src/test/java/net/benas/cb4j/core/test/ReflectionPerformanceShowcase.java
@@ -26,8 +26,6 @@ package net.benas.cb4j.core.test;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -119,36 +117,5 @@ public class ReflectionPerformanceShowcase {
         return person;
     }
 
-}
-
-class Person {
-
-    private String firstName;
-
-    private String lastName;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                '}';
-    }
 }
 
