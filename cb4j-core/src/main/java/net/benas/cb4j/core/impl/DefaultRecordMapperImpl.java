13c9c70b799b2575209f8f50fd5635f09fba1bfd
benas <md.benhassine@gmail.com>
2013-09-11 01:31:12 +0200

    externalize type conversion utility in a dedicated class

--- cb4j-core/src/main/java/net/benas/cb4j/core/impl/DefaultRecordMapperImpl.java
+++ cb4j-core/src/main/java/net/benas/cb4j/core/impl/DefaultRecordMapperImpl.java
@@ -28,18 +28,11 @@ import net.benas.cb4j.core.api.RecordMapper;
 import net.benas.cb4j.core.api.RecordMappingException;
 import net.benas.cb4j.core.model.Field;
 import net.benas.cb4j.core.model.Record;
+import net.benas.cb4j.core.util.TypeConversionUtil;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.sql.Date;
-import java.sql.Time;
-import java.sql.Timestamp;
-import java.util.Calendar;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * A default mapper implementation which uses headers to map values to record field.
@@ -98,7 +91,7 @@ public class DefaultRecordMapperImpl implements RecordMapper {
                 int index = field.getIndex();
 
                 //convert the String raw value to field type
-                Object typedValue = convertValue( content, recordClassSetters[index].getParameterTypes()[0]);
+                Object typedValue = TypeConversionUtil.convertValue(content, recordClassSetters[index].getParameterTypes()[0]);
 
                 //set the typed value to the object field
                 recordClassSetters[index].invoke(instance, typedValue);
@@ -120,88 +113,4 @@ public class DefaultRecordMapperImpl implements RecordMapper {
         return instance;
     }
 
-    /**
-     * Convert raw data from String to typed value.
-     * @param value the String value to convert
-     * @param type the target type
-     * @return Converted value
-     */
-    private Object convertValue(String value, Class type) {
-
-        /*
-         * String and Character types
-         */
-        if (type.equals(String.class)) {
-            return value;
-        }
-        if (type.equals(Character.TYPE) || type.equals(Character.class)) {
-            return value.charAt(0);
-        }
-
-        /*
-         * Boolean type
-         */
-        if (type.equals(Boolean.TYPE) || type.equals(Boolean.class)) {
-            return Boolean.valueOf(value);
-        }
-
-        /*
-         * Numeric types
-         */
-        if (type.equals(Byte.TYPE) || type.equals(Byte.class)) {
-            return Byte.valueOf(value);
-        }
-        if (type.equals(Short.TYPE) || type.equals(Short.class)) {
-            return Short.valueOf(value);
-        }
-        if (type.equals(Integer.TYPE) || type.equals(Integer.class)) {
-            return Integer.valueOf(value);
-        }
-        if (type.equals(Long.TYPE) || type.equals(Long.class)) {
-            return Long.valueOf(value);
-        }
-        if (type.equals(Double.TYPE) || type.equals(Double.class)) {
-            return Double.valueOf(value);
-        }
-        if (type.equals(Float.TYPE) || type.equals(Float.class)) {
-            return Float.valueOf(value);
-        }
-        if (type.equals(BigInteger.class)) {
-            return new BigInteger(value);
-        }
-        if (type.equals(BigDecimal.class)) {
-            return new BigDecimal(value);
-        }
-        if (type.equals(AtomicLong.class)) {
-            return new AtomicLong(Long.valueOf(value));
-        }
-        if (type.equals(AtomicInteger.class)) {
-            return new AtomicInteger(Integer.valueOf(value));
-        }
-
-        /*
-         * Date and time types
-         */
-        if (type.equals(java.util.Date.class)) {
-            return new java.util.Date(Date.parse(value));
-        }
-        if (type.equals(java.sql.Date.class)) {
-            return Date.valueOf(value);
-        }
-        if (type.equals(java.sql.Time.class)) {
-            return Time.valueOf(value);
-        }
-        if (type.equals(java.sql.Timestamp.class)) {
-            return Timestamp.valueOf(value);
-        }
-        if (type.equals(Calendar.class)) {
-            return Calendar.getInstance();
-        }
-
-        /*
-         * Return null for any unsupported type
-         */
-        return null;
-
-    }
 }
