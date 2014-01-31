45aabd59b1630d11dd1161489bf04c2944ccb4cd
benas <md.benhassine@gmail.com>
2014-01-31 01:34:07 +0100

    add csv and xml benchmark

---
+++ easybatch-tutorials/src/main/java/io/github/benas/easybatch/tutorials/bench/Benchmark.java
@@ -0,0 +1,64 @@
+package io.github.benas.easybatch.tutorials.bench;
+
+import io.github.benas.easybatch.core.impl.EasyBatchEngine;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class used to benchmark Easy Batch performance.
+ * Only record reading, parsing and mapping to domain objects are benchmarked.
+ *
+ * @author benas (md.benhassine@gmail.com)
+ */
+public class Benchmark {
+
+    public static void main(String[] args) throws Exception {
+
+        String workDir = args[0];
+        String fileFormat = args[1];
+        int limit = Integer.parseInt(args[2]);
+        String customersFile = workDir + "customers." + fileFormat;
+        Map<Integer, Long> processingTimes = new HashMap<Integer, Long>();
+
+        for (int customersCount = 1000; customersCount < limit + 1; customersCount += (customersCount * 9)) {
+
+            System.out.println("#####################################################");
+            System.out.println("Generating " + customersCount + " random customers...");
+            if ("csv".equalsIgnoreCase(fileFormat)) {
+                BenchmarkUtil.generateCsvCustomers(customersFile, customersCount);
+            } else {
+                BenchmarkUtil.generateXmlCustomers(customersFile, customersCount);
+            }
+
+            // Build an easy batch engine
+            EasyBatchEngine easyBatchEngine;
+            if ("csv".equalsIgnoreCase(fileFormat)) {
+                easyBatchEngine = BenchmarkUtil.buildCsvEasyBatchEngine(customersFile);
+            } else {
+                easyBatchEngine = BenchmarkUtil.buildXmlEasyBatchEngine(customersFile);
+            }
+            System.out.println("Running easy batch...");
+            long startTime = System.currentTimeMillis();
+            easyBatchEngine.call();
+            long duration = System.currentTimeMillis() - startTime;
+            System.out.println("Easy batch engine took = " + duration + "ms to process " + customersCount + " customers");
+            processingTimes.put(customersCount, duration);
+
+        }
+
+        System.out.println("###################");
+        System.out.println("Benchmark results :");
+        for (Integer customersCount : processingTimes.keySet()) {
+            System.out.println("Easy batch engine took = " + processingTimes.get(customersCount) + "ms to process " +
+                    customersCount + " customers");
+        }
+
+        //remove generated file
+        File file = new File(customersFile);
+        //file.delete();
+
+    }
+
+}
---
+++ easybatch-tutorials/src/main/java/io/github/benas/easybatch/tutorials/bench/Customer.java
@@ -0,0 +1,102 @@
+package io.github.benas.easybatch.tutorials.bench;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+/**
+ * Customer bean used for benchmark.
+ */
+@XmlRootElement
+public class Customer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String email;
+    private String phone;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
