1a402569a8af4843ad51c8eeef83f6eb144f4ea0
benas <md.benhassine@gmail.com>
2014-02-07 07:03:29 +0100

    add tests for XmlRecordMapper

---
+++ easybatch-xml/src/test/java/io/github/benas/easybatch/xml/test/XmlRecordMapperTest.java
@@ -0,0 +1,50 @@
+package io.github.benas.easybatch.xml.test;
+
+import io.github.benas.easybatch.xml.XmlRecord;
+import io.github.benas.easybatch.xml.XmlRecordMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class for {@link XmlRecordMapper}.
+ *
+ * @author benas (md.benhassine@gmail.com)
+ */
+public class XmlRecordMapperTest {
+
+    private XmlRecordMapper xmlRecordMapper;
+
+    private XmlRecord xmlRecord;
+
+    @Before
+    public void setUp() throws Exception {
+        xmlRecordMapper = new XmlRecordMapper<Person>(Person.class);
+        String xml = new Scanner(this.getClass().getResourceAsStream("/person.xml") ).useDelimiter("\\A").next();
+        xmlRecord = new XmlRecord(1, xml);
+    }
+
+    @Test
+    public void testXmlPersonMapping() throws Exception {
+        Person person = (Person) xmlRecordMapper.mapRecord(xmlRecord);
+        assertEquals(1, person.getId());
+        assertEquals("foo", person.getFirstName());
+        assertEquals("bar", person.getLastName());
+//        assertEquals(new SimpleDateFormat("yyyy-MM-dd").parse("2000-01-01"), person.getBirthDate());
+        assertEquals(true, person.isMarried());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        xmlRecord = null;
+        xmlRecordMapper = null;
+        System.gc();
+    }
+}
