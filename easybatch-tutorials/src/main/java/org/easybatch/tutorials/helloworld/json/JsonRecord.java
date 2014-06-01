61e86ff4ae4eed18982ae48ccdc8944eabe9dcc6
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-06-01 14:35:31 +0200

    add tutorial to process JSON data

---
+++ easybatch-tutorials/src/main/java/org/easybatch/tutorials/helloworld/json/JsonRecord.java
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ *   Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.tutorials.helloworld.json;
+
+import org.easybatch.core.util.StringRecord;
+
+/**
+ * A class representing a Json record.
+ *
+ * @author Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ */
+public class JsonRecord extends StringRecord {
+
+    /**
+     * Constructs a JsonRecord instance.
+     * @param recordNumber the record number
+     * @param rawContent the record raw Json content
+     */
+    public JsonRecord(int recordNumber, String rawContent) {
+        super(recordNumber, rawContent);
+    }
+
+}
