92f1cd081bc9ec34cc676b518fa9f7e14dd10bc6
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-12-23 22:35:52 +0100

    update author's home page and email

--- easybatch-tutorials/src/main/java/org/easybatch/tutorials/customers/etl/CustomerProcessor.java
+++ easybatch-tutorials/src/main/java/org/easybatch/tutorials/customers/etl/CustomerProcessor.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- *  Copyright (c) 2014, Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ *  Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@ import javax.xml.bind.Marshaller;
 /**
  * A customer processor implementation that generates customer XML data using JAXB.
  *
- * @author Mahmoud Ben Hassine (md.benhassine@gmail.com)
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
  */
 public class CustomerProcessor implements RecordProcessor<Customer, Customer> {
 
