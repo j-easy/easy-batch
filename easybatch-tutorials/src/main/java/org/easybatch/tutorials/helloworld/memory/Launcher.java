4f8d907c5c9c1cf200ccd652c9895e08f695731c
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2014-12-24 21:29:01 +0100

    remove unused filter

--- easybatch-tutorials/src/main/java/org/easybatch/tutorials/helloworld/memory/Launcher.java
+++ easybatch-tutorials/src/main/java/org/easybatch/tutorials/helloworld/memory/Launcher.java
@@ -25,7 +25,6 @@
 package org.easybatch.tutorials.helloworld.memory;
 
 import org.easybatch.core.api.Report;
-import org.easybatch.core.filter.StartWithStringRecordFilter;
 import org.easybatch.core.impl.Engine;
 import org.easybatch.core.impl.EngineBuilder;
 import org.easybatch.core.util.StringRecordReader;
@@ -51,7 +50,6 @@ public class Launcher {
         // Build a batch engine
         Engine engine = new EngineBuilder()
                 .readRecordsWith(new StringRecordReader(dataSource))
-                .filterRecordsWith(new StartWithStringRecordFilter("#"))
                 .mapRecordsWith(new DelimitedRecordMapper<Greeting>(Greeting.class, new String[]{"id", "name"}))
                 .processRecordsWith(new GreetingTransformer())
                 .thenWith(new GreetingProcessor())
