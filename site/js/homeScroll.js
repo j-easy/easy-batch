3d61f7cbb51319cda24e536cf159c18196f141ba
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-01-04 19:18:56 +0100

    update documentation

---
+++ site/js/homeScroll.js
@@ -0,0 +1,22 @@
+$(document).ready(function (){
+    $("#learnMore").click(function (){
+        $(this).blur();
+        $('html, body').animate({
+            scrollTop: $("#motivation").offset().top - 150
+        }, 700);
+    });
+
+    $("#showCode").click(function (){
+        $(this).blur();
+        $('html, body').animate({
+            scrollTop: $("#code").offset().top - 150
+        }, 700);
+    });
+
+    $("#showFeatures").click(function (){
+        $(this).blur();
+        $('html, body').animate({
+            scrollTop: $("#features").offset().top - 150
+        }, 700);
+    });
+});
--- site/js/learnMore.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-    $("#learnMore").click(function (){
-        $(this).blur();
-        $('html, body').animate({
-            scrollTop: $("#features").offset().top - 150
-        }, 700);
-    });
-});
