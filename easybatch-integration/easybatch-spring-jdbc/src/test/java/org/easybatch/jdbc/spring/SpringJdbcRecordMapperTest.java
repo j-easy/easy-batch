a38d67466fd18a47616c5f41a68bf2113996d504
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-02-12 23:03:30 +0100

    add tests for SpringJdbcRecordMapper

---
+++ easybatch-integration/easybatch-spring-jdbc/src/test/java/org/easybatch/jdbc/spring/SpringJdbcRecordMapperTest.java
@@ -0,0 +1,61 @@
+package org.easybatch.jdbc.spring;
+
+import org.easybatch.jdbc.JdbcRecord;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Types;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test class for {@link SpringJdbcRecordMapper}.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SpringJdbcRecordMapperTest {
+
+    private SpringJdbcRecordMapper<Tweet> mapper;
+
+    private JdbcRecord jdbcRecord;
+
+    @Mock
+    private ResultSet payload;
+    @Mock
+    private ResultSetMetaData metadata;
+
+    @Before
+    public void setUp() throws Exception {
+        mapper = new SpringJdbcRecordMapper<Tweet>(Tweet.class);
+        jdbcRecord = new JdbcRecord(1, payload);
+    }
+
+    @Test
+    public void testMapRecord() throws Exception {
+        when(payload.getMetaData()).thenReturn(metadata);
+        when(metadata.getColumnCount()).thenReturn(3);
+        when(metadata.getColumnLabel(1)).thenReturn("id");
+        when(metadata.getColumnType(1)).thenReturn(Types.INTEGER);
+        when(metadata.getColumnLabel(2)).thenReturn("user");
+        when(metadata.getColumnType(2)).thenReturn(Types.VARCHAR);
+        when(metadata.getColumnLabel(3)).thenReturn("message");
+        when(metadata.getColumnType(3)).thenReturn(Types.VARCHAR);
+        when(payload.getInt(1)).thenReturn(1);
+        when(payload.getString(2)).thenReturn("foo");
+        when(payload.getString(3)).thenReturn("message");
+
+        Tweet tweet = mapper.mapRecord(jdbcRecord);
+
+        assertThat(tweet).isNotNull();
+        assertThat(tweet.getId()).isEqualTo(1);
+        assertThat(tweet.getUser()).isEqualTo("foo");
+        assertThat(tweet.getMessage()).isEqualTo("message");
+    }
+}
