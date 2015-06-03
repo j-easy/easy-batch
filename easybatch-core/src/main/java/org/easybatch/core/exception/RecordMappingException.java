9a7cd7daa334e7deaba5e544849ec7097338aad9
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-06-03 20:40:31 +0200

    add license header

--- easybatch-core/src/main/java/org/easybatch/core/exception/RecordMappingException.java
+++ easybatch-core/src/main/java/org/easybatch/core/exception/RecordMappingException.java
@@ -22,62 +22,16 @@
  *   THE SOFTWARE.
  */
 
-package org.easybatch.jdbc;
+package org.easybatch.core.exception;
 
 /**
- * Java bean representing a tweet.
+ * Exception thrown when an error occurs during record mapping.
  *
  * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
  */
-public class Tweet {
+public class RecordMappingException extends Exception {
 
-    private int id;
-
-    private String user;
-
-    private String message;
-
-    public Tweet() {
+    public RecordMappingException(String message, Throwable cause) {
+        super(message, cause);
     }
-
-    public Tweet(int id, String user, String message) {
-        this.id = id;
-        this.user = user;
-        this.message = message;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("Tweet{");
-        sb.append("id=").append(id);
-        sb.append(", user='").append(user).append('\'');
-        sb.append(", message='").append(message).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
-
 }
