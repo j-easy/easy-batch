14eeb26c1719c8fad5dc4cc278723067f9cc7c54
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-06 23:52:27 +0200

    implement feature #79 : Add OutputStreamRecordWriter

---
+++ easybatch-core/src/main/java/org/easybatch/core/processor/OutputStreamRecordWriter.java
@@ -0,0 +1,74 @@
+/*
+ *  The MIT License
+ *
+ *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.core.processor;
+
+import org.easybatch.core.api.RecordProcessingException;
+import org.easybatch.core.api.RecordProcessor;
+import org.easybatch.core.record.StringRecord;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import static java.lang.String.format;
+import static org.easybatch.core.util.Utils.checkNotNull;
+
+/**
+ * Convenient processor to write the <strong>payload</strong> of a {@link StringRecord} to an output stream.
+ * <p/>
+ * The user of this class is responsible for opening/closing the output stream, maybe using
+ * a {@link org.easybatch.core.api.event.job.JobEventListener}.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+public class OutputStreamRecordWriter implements RecordProcessor<StringRecord, StringRecord> {
+
+    private OutputStreamWriter outputStreamWriter;
+
+    /**
+     * Convenient processor to write the <strong>payload</strong> of a {@link StringRecord} to an output stream.
+     * <p/>
+     * The user of this class is responsible for opening/closing the output stream, maybe using
+     * a {@link org.easybatch.core.api.event.job.JobEventListener}.
+     *
+     * @param outputStreamWriter the output stream to write records to.
+     */
+    public OutputStreamRecordWriter(OutputStreamWriter outputStreamWriter) {
+        checkNotNull(outputStreamWriter, "output stream writer");
+        this.outputStreamWriter = outputStreamWriter;
+    }
+
+    @Override
+    public StringRecord processRecord(StringRecord record) throws RecordProcessingException {
+        try {
+            outputStreamWriter.write(record.getPayload() + "\n");
+            outputStreamWriter.flush();
+            return record;
+        } catch (IOException exception) {
+            String message = format("Unable to write record %s to the output stream writer", record);
+            throw new RecordProcessingException(message, exception);
+        }
+    }
+
+}
