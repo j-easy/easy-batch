dc98a09aa3b559a15f9f32cb2c5265346103f684
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-07 09:15:29 +0200

    implement feature #82 : Add MongoDBRecordWriter

---
+++ easybatch-integration/easybatch-mongodb/src/main/java/org/easybatch/integration/mongodb/MongoDBRecordWriter.java
@@ -0,0 +1,60 @@
+/*
+ *  The MIT License
+ *
+ *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.integration.mongodb;
+
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import org.easybatch.core.api.RecordProcessingException;
+import org.easybatch.core.api.RecordProcessor;
+
+/**
+ * Writes a Mongo {DBObject} in a given collection.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+public class MongoDBRecordWriter implements RecordProcessor<DBObject, DBObject> {
+
+    private DBCollection collection;
+
+    /**
+     * Create a MongoDB writer.
+     *
+     * @param collection the collection to write documents to.
+     */
+    public MongoDBRecordWriter(DBCollection collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public DBObject processRecord(DBObject record) throws RecordProcessingException {
+        try {
+            collection.save(record);
+            return record;
+        } catch (Exception e) {
+            throw new RecordProcessingException("Unable to write document to MongoDB server", e);
+        }
+    }
+
+}
