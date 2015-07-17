69f4ecf3692f2a7bc882f8accdce198b86b226cc
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-17 21:45:26 +0200

    implement feature #99 : Add GsonRecordMarshaller

---
+++ easybatch-integration/easybatch-gson/src/test/java/org/easybatch/integration/gson/GsonRecordMarshallerTest.java
@@ -0,0 +1,56 @@
+/*
+ *  The MIT License
+ *
+ *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.integration.gson;
+
+import org.easybatch.core.api.RecordMarshallingException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for {@link GsonRecordMarshaller}.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+public class GsonRecordMarshallerTest {
+
+    private GsonRecordMarshaller marshaller;
+
+    @Before
+    public void setUp() {
+        marshaller = new GsonRecordMarshaller();
+    }
+
+    @Test
+    public void marshal() throws RecordMarshallingException {
+        Tweet tweet = new Tweet(1, "foo", "hi");
+
+        String expected = "{\"id\":1,\"user\":\"foo\",\"message\":\"hi\"}";
+        String actual = marshaller.marshal(tweet);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
