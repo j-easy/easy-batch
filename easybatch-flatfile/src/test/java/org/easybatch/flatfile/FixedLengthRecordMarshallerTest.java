fe7fdf311a9759a3ddd2450c543663751075f004
Mahmoud Ben Hassine <md.benhassine@gmail.com>
2015-07-17 21:22:28 +0200

    implement feature #97 : Add FixedLengthRecordMarshaller

---
+++ easybatch-flatfile/src/test/java/org/easybatch/flatfile/FixedLengthRecordMarshallerTest.java
@@ -0,0 +1,58 @@
+/*
+ *  The MIT License
+ *
+ *   Copyright (c) 2015, Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.flatfile;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Test class for {@link FixedLengthRecordMarshaller}.
+ * 
+ * @author Mahmoud Ben Hassine (mahmoud@benhassine.fr)
+ */
+public class FixedLengthRecordMarshallerTest {
+
+    private FixedLengthRecordMarshaller marshaller;
+    
+    @Before
+    public void setUp() throws Exception {
+        marshaller = new FixedLengthRecordMarshaller(Bean.class, new String[] {"field1", "field2", "field3"});
+    }
+
+    @Test
+    public void marshal() throws Exception {
+        Bean bean = new Bean();
+        bean.setField1("aaa");
+        bean.setField2("bb");
+        bean.setField3("cccc");
+
+        String expected = "aaabbcccc";
+        String actual = marshaller.marshal(bean);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
