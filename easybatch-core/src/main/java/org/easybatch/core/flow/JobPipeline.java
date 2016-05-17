4dc8b9943c4a36d8a8b9a109259152c9d4e5b9bc
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-05-17 13:51:02 +0200

    fix issue with ordering in job pipeline

--- easybatch-core/src/main/java/org/easybatch/core/flow/JobPipeline.java
+++ easybatch-core/src/main/java/org/easybatch/core/flow/JobPipeline.java
@@ -4,31 +4,34 @@ import org.easybatch.core.flow.predicate.JobExecutionPredicate;
 import org.easybatch.core.job.Job;
 import org.easybatch.core.job.JobReport;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Callable;
 
 public class JobPipeline implements Callable<JobReport> {
 
-    private Map<Job, JobExecutionPredicate> jobs = new HashMap<>();
+    private final List<Job> jobs = new ArrayList<>();
+
+    private final List<JobExecutionPredicate> jobExecutionPredicates = new ArrayList<>();
 
     public JobPipeline(List<Job> jobs, List<JobExecutionPredicate> jobExecutionPredicates) {
-        for (int i = 0; i < jobExecutionPredicates.size(); i++) {
-            this.jobs.put(jobs.get(i), jobExecutionPredicates.get(i));
-        }
-        this.jobs.put(jobs.get(jobs.size() - 1), null); // the last job does not have a predicate
+        this.jobs.addAll(jobs);
+        this.jobExecutionPredicates.addAll(jobExecutionPredicates);
     }
 
     @Override
     public JobReport call() throws Exception {
         // TODO sanity checks
         JobReport jobReport = null;
-        for (Job job : jobs.keySet()) {
+        int i = 0;
+        for (Job job : jobs) {
             jobReport = job.call();
-            JobExecutionPredicate jobExecutionPredicate = jobs.get(job);
-            if (jobExecutionPredicate != null && !jobExecutionPredicate.apply(jobReport)) {
+            if (i < jobExecutionPredicates.size()) {
+                JobExecutionPredicate jobExecutionPredicate = jobExecutionPredicates.get(i);
+                i++;
+                if (!jobExecutionPredicate.apply(jobReport)) {
                     break;
+                }
             }
         }
         return jobReport;
--- easybatch-core/src/test/java/org/easybatch/core/flow/JobPipelineTest.java
+++ easybatch-core/src/test/java/org/easybatch/core/flow/JobPipelineTest.java
@@ -4,14 +4,15 @@ import org.easybatch.core.job.Job;
 import org.easybatch.core.job.JobReport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.easybatch.core.flow.JobPipelineBuilder.aNewJobPipelineBuilder;
 import static org.easybatch.core.flow.predicate.JobCompleted.jobCompleted;
+import static org.easybatch.core.flow.predicate.JobFailed.jobFailed;
 import static org.easybatch.core.job.JobStatus.COMPLETED;
+import static org.easybatch.core.job.JobStatus.FAILED;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -19,25 +20,29 @@ import static org.mockito.Mockito.when;
 public class JobPipelineTest {
 
     @Mock
-    private Job job1, job2;
+    private Job job1, job2, job3;
     @Mock
-    private JobReport jobReport1, jobReport2;
+    private JobReport jobReport1, jobReport2, jobReport3;
 
     @Test
     public void testJobPipeline() throws Exception {
         when(job1.call()).thenReturn(jobReport1);
         when(jobReport1.getStatus()).thenReturn(COMPLETED);
         when(job2.call()).thenReturn(jobReport2);
+        when(jobReport2.getStatus()).thenReturn(FAILED);
+        when(job3.call()).thenReturn(jobReport3);
 
         JobPipeline jobPipeline = aNewJobPipelineBuilder()
                 .startWith(job1)
                 .when(jobCompleted()).then(job2)
+                .when(jobFailed()).then(job3)
                 .build();
 
         JobReport lastJobReport = jobPipeline.call();
 
         verify(job1).call();
         verify(job2).call();
-        assertThat(lastJobReport).isEqualTo(jobReport2);
+        verify(job3).call();
+        assertThat(lastJobReport).isEqualTo(jobReport3);
     }
 }
