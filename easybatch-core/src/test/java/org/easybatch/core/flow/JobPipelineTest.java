e38b828a3b32e5b8d420845da166742df7cef199
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-05-17 17:39:04 +0200

    add integration test for JobPipeline

--- easybatch-core/src/test/java/org/easybatch/core/flow/JobPipelineTest.java
+++ easybatch-core/src/test/java/org/easybatch/core/flow/JobPipelineTest.java
@@ -1,18 +1,35 @@
 package org.easybatch.core.flow;
 
+import org.easybatch.core.dispatcher.PoisonRecordBroadcaster;
+import org.easybatch.core.filter.PoisonRecordFilter;
 import org.easybatch.core.job.Job;
 import org.easybatch.core.job.JobReport;
+import org.easybatch.core.reader.BlockingQueueRecordReader;
+import org.easybatch.core.reader.StringRecordReader;
+import org.easybatch.core.record.Record;
+import org.easybatch.core.record.StringRecord;
+import org.easybatch.core.writer.BlockingQueueRecordWriter;
+import org.easybatch.core.writer.CollectionRecordWriter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.easybatch.core.flow.JobPipelineBuilder.aNewJobPipelineBuilder;
 import static org.easybatch.core.flow.predicate.JobCompleted.jobCompleted;
 import static org.easybatch.core.flow.predicate.JobFailed.jobFailed;
+import static org.easybatch.core.job.JobBuilder.aNewJob;
 import static org.easybatch.core.job.JobStatus.COMPLETED;
 import static org.easybatch.core.job.JobStatus.FAILED;
+import static org.easybatch.core.util.Utils.LINE_SEPARATOR;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -44,5 +61,37 @@ public class JobPipelineTest {
         verify(job2).call();
         verify(job3).call();
         assertThat(lastJobReport).isEqualTo(jobReport3);
+    }
+
+    @Test
+    public void integrationTest() throws Exception {
+        // Given
+        String dataSource = "foo" + LINE_SEPARATOR + "bar";
+        BlockingQueue<Record> buffer = new LinkedBlockingQueue<>();
+        List<String> records = new ArrayList<>();
+
+        Job job1 = aNewJob()
+                .named("job1")
+                .reader(new StringRecordReader(dataSource))
+                .writer(new BlockingQueueRecordWriter<>(buffer))
+                .jobListener(new PoisonRecordBroadcaster<>(singletonList(buffer)))
+                .build();
+        Job job2 = aNewJob()
+                .named("job2")
+                .reader(new BlockingQueueRecordReader<>(buffer))
+                .filter(new PoisonRecordFilter())
+                .writer(new CollectionRecordWriter(records))
+                .build();
+
+        // When
+        JobPipeline jobPipeline = aNewJobPipelineBuilder()
+                .startWith(job1)
+                .when(jobCompleted()).then(job2)
+                .build();
+        JobReport lastJobReport = jobPipeline.call();
+
+        // Then
+        assertThat(lastJobReport).isNotNull();
+        assertThat(records).hasSize(2);
     }
 }
