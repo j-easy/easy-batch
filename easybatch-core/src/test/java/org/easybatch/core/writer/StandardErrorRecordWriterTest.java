22ce8514135b81f14321a4b3611356dc0b8c77ce
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-09-08 21:21:38 +0200

    issue #208 : add StandardErrorRecordWriter

---
+++ easybatch-core/src/test/java/org/easybatch/core/writer/StandardErrorRecordWriterTest.java
@@ -0,0 +1,70 @@
+/*
+ *  The MIT License
+ *
+ *   Copyright (c) 2016, Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+package org.easybatch.core.writer;
+
+import org.easybatch.core.record.Record;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.easybatch.core.util.Utils.LINE_SEPARATOR;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test class for {@link StandardErrorRecordWriter}.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class StandardErrorRecordWriterTest {
+
+    public static final String PAYLOAD = "Foo";
+
+    @Rule
+    public final SystemErrRule systemErr = new SystemErrRule().enableLog();
+
+    @Mock
+    private Record record;
+
+    private StandardErrorRecordWriter writer;
+
+    @Before
+    public void setUp() throws Exception {
+        when(record.getPayload()).thenReturn(PAYLOAD);
+        writer = new StandardErrorRecordWriter();
+    }
+
+    @Test
+    public void testRecordPayloadWriting() throws Exception {
+        writer.processRecord(record);
+
+        assertThat(systemErr.getLog()).isEqualTo(PAYLOAD + LINE_SEPARATOR);
+    }
+}
