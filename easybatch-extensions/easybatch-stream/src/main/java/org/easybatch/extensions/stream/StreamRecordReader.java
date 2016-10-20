7860b5d959853fc7e1249276775717c54fbf027e
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2016-10-20 20:49:38 +0200

    update javadoc

--- easybatch-extensions/easybatch-stream/src/main/java/org/easybatch/extensions/stream/StreamRecordReader.java
+++ easybatch-extensions/easybatch-stream/src/main/java/org/easybatch/extensions/stream/StreamRecordReader.java
@@ -10,75 +10,80 @@ import java.util.Iterator;
 import java.util.stream.Stream;
 
 /**
+ * Reader that reads records form a {@link Stream}.
+ *
+ * This reader produces {@link GenericRecord} instances.
+ *
+ * @param <T> Type of elements in the stream.
  * @author Charles Fleury
- * @since 17/10/16.
+ * @since 5.0
  */
 public class StreamRecordReader<T> implements RecordReader {
 
-  private static final String DEFAULT_DATASOURCE_NAME = "DATASOURCE";
+    private static final String DEFAULT_DATASOURCE_NAME = "DATASOURCE";
 
-  protected String datasource;
-  protected Stream<T> stream;
-  protected Iterator<T> iterator;
-  protected long currentRecordNumber;
+    protected String datasource;
+    protected Stream<T> stream;
+    protected Iterator<T> iterator;
+    protected long currentRecordNumber;
 
-  /**
-   * Create a {@link StreamRecordReader} to read record from java 8 stream.
-   *
-   * @param stream to read record from
-   */
-  public StreamRecordReader(final Stream<T> stream) {
-    this(stream, DEFAULT_DATASOURCE_NAME);
-  }
-
-  /**
-   * Create a {@link StreamRecordReader} to read record from java 8 stream.
-   *
-   * @param stream to read record from
-   *  @param datasource name (default to DEFAULT_DATASOURCE_NAME)
-   */
-  public StreamRecordReader(final Stream<T> stream, final String datasource) {
-    this.stream = stream;
-    this.datasource = datasource;
-  }
-
-  /**
-   * Open the reader.
-   */
-  @Override
-  public void open() throws Exception {
-    if(stream == null) {
-      throw new IllegalArgumentException("stream must not be null");
+    /**
+     * Create a {@link StreamRecordReader} to read record from a {@link Stream}.
+     *
+     * @param stream to read record from
+     */
+    public StreamRecordReader(final Stream<T> stream) {
+        this(stream, DEFAULT_DATASOURCE_NAME);
     }
 
-    if(datasource == null || datasource.isEmpty()) {
-      datasource = DEFAULT_DATASOURCE_NAME;
+    /**
+     * Create a {@link StreamRecordReader} to read record from a {@link Stream}.
+     *
+     * @param stream     to read record from
+     * @param datasource name (default to DEFAULT_DATASOURCE_NAME)
+     */
+    public StreamRecordReader(final Stream<T> stream, final String datasource) {
+        this.stream = stream;
+        this.datasource = datasource;
     }
 
-    currentRecordNumber = 0;
-    iterator = stream.iterator();
-  }
+    /**
+     * Open the reader.
+     */
+    @Override
+    public void open() throws Exception {
+        if (stream == null) {
+            throw new IllegalArgumentException("stream must not be null");
+        }
 
-  /**
-   * Read next record from the data source.
-   *
-   * @return the next record from the data source.
-   */
-  @Override
-  public Record<T> readRecord() throws Exception {
-    if(iterator.hasNext()) {
-      Header header = new Header(++currentRecordNumber, datasource, new Date());
-      return new GenericRecord<>(header, iterator.next());
-    } else {
-      return null;
+        if (datasource == null || datasource.isEmpty()) {
+            datasource = DEFAULT_DATASOURCE_NAME;
+        }
+
+        currentRecordNumber = 0;
+        iterator = stream.iterator();
     }
-  }
 
-  /**
-   * Close the reader.
-   */
-  @Override
-  public void close() throws Exception {
-    // no op
-  }
+    /**
+     * Read next record from the data source.
+     *
+     * @return the next record from the data source.
+     */
+    @Override
+    public Record<T> readRecord() throws Exception {
+        if (iterator.hasNext()) {
+            Header header = new Header(++currentRecordNumber, datasource, new Date());
+            return new GenericRecord<>(header, iterator.next());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Close the reader.
+     */
+    @Override
+    public void close() throws Exception {
+        // no op
+    }
 }
