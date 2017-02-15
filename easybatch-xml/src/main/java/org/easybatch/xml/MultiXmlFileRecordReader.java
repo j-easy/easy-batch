9ebf8e37fdbc0580e245c9de79722a45de71f71e
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2017-02-15 21:35:10 +0100

    issue #254 : add MultiXmlFileRecordReader

---
+++ easybatch-xml/src/main/java/org/easybatch/xml/MultiXmlFileRecordReader.java
@@ -0,0 +1,32 @@
+package org.easybatch.xml;
+
+import org.easybatch.core.reader.AbstractFileRecordReader;
+import org.easybatch.core.reader.AbstractMultiFileRecordReader;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Reader for multiple xml files in one shot.
+ *
+ * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
+ */
+public class MultiXmlFileRecordReader extends AbstractMultiFileRecordReader {
+
+    private String rootElementName;
+
+    /**
+     * Create a new {@link MultiXmlFileRecordReader}.
+     *
+     * @param files to read
+     */
+    public MultiXmlFileRecordReader(List<File> files, String rootElementName) {
+        super(files);
+        this.rootElementName = rootElementName;
+    }
+
+    @Override
+    protected AbstractFileRecordReader createReader() throws Exception {
+        return new XmlFileRecordReader(rootElementName, currentFile);
+    }
+}
--- easybatch-xml/src/test/java/org/easybatch/xml/MultiXmlFileRecordReaderTest.java
+++ easybatch-xml/src/test/java/org/easybatch/xml/MultiXmlFileRecordReaderTest.java
@@ -4,26 +4,24 @@ import org.easybatch.core.job.Job;
 import org.easybatch.core.job.JobBuilder;
 import org.easybatch.core.job.JobExecutor;
 import org.easybatch.core.processor.RecordCollector;
-import org.easybatch.core.reader.MultiFileRecordReader;
 import org.easybatch.core.record.Record;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MultiXmlFileRecordReaderTest {
 
-    @Ignore("Fails because XmlFileRecordWriter should be created with root element name parameter in addition to the xml file")
     @Test
     public void allResourcesShouldBeReadInOneShot() throws Exception {
-        MultiFileRecordReader multiFileRecordReader = new MultiFileRecordReader(
-                new File("src/test/resources"),
-                new XmlFileFilter(),
-                XmlFileRecordReader.class);
+        // given
+        File dir = new File("src/test/resources");
+        File[] files = dir.listFiles(new XmlFileFilter());
+        MultiXmlFileRecordReader multiFileRecordReader = new MultiXmlFileRecordReader(Arrays.asList(files), "website");
 
         RecordCollector recordCollector = new RecordCollector();
         Job job = JobBuilder.aNewJob()
@@ -31,12 +29,14 @@ public class MultiXmlFileRecordReaderTest {
                 .processor(recordCollector)
                 .build();
 
+        // when
         JobExecutor jobExecutor = new JobExecutor();
         jobExecutor.execute(job);
         jobExecutor.shutdown();
 
         List<Record> records = recordCollector.getRecords();
 
+        // then
         // there are 4 records in xml files starting with "web" inside "src/test/resources"
         assertThat(records).hasSize(4);
 
