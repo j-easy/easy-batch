87df1fbb820639d1f864dcc5803dc94a6d8769a1
Malte Pickhan <noreply@reply.github.bus.zalan.do>
2018-04-12 21:09:45 +0200

    Change logger in JdbcRecordReader to Slf4j

--- easybatch-jdbc/src/main/java/org/easybatch/jdbc/JdbcRecordReader.java
+++ easybatch-jdbc/src/main/java/org/easybatch/jdbc/JdbcRecordReader.java
@@ -25,6 +25,8 @@ package org.easybatch.jdbc;
 
 import org.easybatch.core.reader.RecordReader;
 import org.easybatch.core.record.Header;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -32,8 +34,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static org.easybatch.core.util.Utils.checkArgument;
 import static org.easybatch.core.util.Utils.checkNotNull;
@@ -47,7 +47,7 @@ import static org.easybatch.core.util.Utils.checkNotNull;
  */
 public class JdbcRecordReader implements RecordReader {
 
-    private static final Logger LOGGER = Logger.getLogger(JdbcRecordReader.class.getSimpleName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcRecordReader.class.getSimpleName());
 
     private DataSource dataSource;
     private Connection connection;
@@ -97,7 +97,7 @@ public class JdbcRecordReader implements RecordReader {
         try {
             return resultSet.next();
         } catch (SQLException e) {
-            LOGGER.log(Level.SEVERE, "An exception occurred during checking the existence of next database record", e);
+            LOGGER.error("An exception occurred during checking the existence of next database record", e);
             return false;
         }
     }
@@ -116,7 +116,7 @@ public class JdbcRecordReader implements RecordReader {
         try {
             return "Connection URL: " + connection.getMetaData().getURL() + " | Query string: " + query;
         } catch (SQLException e) {
-            LOGGER.log(Level.SEVERE, "Unable to get data source name", e);
+            LOGGER.error("Unable to get data source name", e);
             return "N/A";
         }
     }
