139c675675b19bab0dfb0a25b8b65f0546120255
Mahmoud Ben Hassine <mahmoud.benhassine@icloud.com>
2020-01-12 18:26:18 +0100

    Update javadoc

--- easybatch-core/src/main/java/org/easybatch/core/filter/GrepFilter.java
+++ easybatch-core/src/main/java/org/easybatch/core/filter/GrepFilter.java
@@ -29,8 +29,9 @@ import org.easybatch.core.util.Utils;
 /**
  * Convenient filter that mimics the unix grep utility: it keeps records containing the given pattern
  * instead of filtering them.
- * <p/>
- * Should be used with {@link org.easybatch.core.record.StringRecord} type. Search is case sensitive.
+ *
+ * Should be used with {@link org.easybatch.core.record.StringRecord} type. Search is based on
+ * {@link String#contains(CharSequence)}.
  *
  * @author Mahmoud Ben Hassine (mahmoud.benhassine@icloud.com)
  */
